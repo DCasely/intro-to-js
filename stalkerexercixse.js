b00964a0ecb459a678990b15d665e46589b4f614
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-02-04 17:52:55 -0500

    initial commit

---
+++ stalkerexercixse.js
@@ -0,0 +1,5 @@
+var firstName = prompt('What is your first name?');
+var lastName = prompt('What is your last name?');
+var age = prompt('How old are you?');
+console.log(firstName + ' ' + lastName + ' ' + 'welcome to the new website.');
+console.log('I see that you are ' + age + ' years old!');
