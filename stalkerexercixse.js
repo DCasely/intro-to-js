2948fb8c9e03da0184eaaba81ca5ec8ec0a0ca1a
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-02-04 18:16:13 -0500

    modified

--- stalkerexercixse.js
+++ stalkerexercixse.js
@@ -1,5 +1,8 @@
 var firstName = prompt('What is your first name?');
 var lastName = prompt('What is your last name?');
 var age = prompt('How old are you?');
-console.log(firstName + ' ' + lastName + ' ' + 'welcome to the new website.');
+
+var fullName = firstName + ' ' + lastName;
+
+console.log(fullName + ' welcome to the new website.');
 console.log('I see that you are ' + age + ' years old!');
